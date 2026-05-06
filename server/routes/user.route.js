5fe9152a779adfac1bed20ecf3ee49037ad6a7ff
Ganesh Devadiga <ganeshmurdeshwar@gmail.com>
2026-05-06 21:18:17 +0530

    Home page , AI-component generater page are created

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,10 +2,12 @@
 
 import express from "express";
 import isAuth from "../middlewares/isAuth.js";
-import { currentUser } from "../controllers/user.controller.js";
+import { currentUser, getAllUsers } from "../controllers/user.controller.js";
 
 const userRouter = express.Router()
 
 userRouter.get("/currentUser", isAuth, currentUser);
+userRouter.get("/all-users",getAllUsers)
+
 
 export default userRouter
